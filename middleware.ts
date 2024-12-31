3f83bad1202b21b18e63f0ac60d7805b6cc60995
swtmply <delosreyesjohnallen@gmail.com>
2024-12-31 15:33:33 +0800

    added authentication middleware

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { betterFetch } from "@better-fetch/fetch";
+import type { auth } from "@/lib/auth";
+import { NextResponse, type NextRequest } from "next/server";
+
+type Session = typeof auth.$Infer.Session;
+
+export default async function authMiddleware(request: NextRequest) {
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        //get the cookie from the request
+        cookie: request.headers.get("cookie") || "",
+      },
+    }
+  );
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (session && request.nextUrl.pathname === "/login") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/login"],
+};
