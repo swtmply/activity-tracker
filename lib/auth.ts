4d1d6f4068327d6f65583a0ee9bb18af25a97b6c
swtmply <delosreyesjohnallen@gmail.com>
2025-01-07 00:46:17 +0800

    updated auth for expo

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,13 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
+import { expo } from "@better-auth/expo";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
+  plugins: [expo()],
   socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
@@ -16,4 +18,5 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  trustedOrigins: ["taptivity://", "exp://"],
 });
